0f90993cbd220609e6d5a141dd3e82411138eb3c
Dalma <p.d.f.1915@gmail.com>
2024-11-24 01:52:34 -0300

    Se agrega ejercicio en lenguaje java

---
+++ java/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,30 @@
+
+package holamundo;
+
+/**
+ *
+ * @author dev
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("¡Hola Mundo!");
+        
+//        asignacion
+        int numero = 24;
+        double precio = 35.45;
+        boolean esValido = true;
+        char vocal = 'a';
+        String nombre = "Maria";
+        long numeroLargo = 215545445;
+        
+        System.out.println("Variables disponibles: " + numero + ", " + precio + ", " + esValido + ", " + vocal + ", " + nombre + ", " + numeroLargo);
+        
+    }
+    
+
+}
